f264b255277140b91f113cf62deac16326d7025d
Ramon Mesa <alu4429@cc426.(none)>
2013-10-16 17:41:49 +0100

    Multiplicación implementada

--- Rakefile
+++
@@ -1,9 +0,0 @@
-desc "build HTML from README.md"
-task :html do
-  sh "kramdown README.md  > README.html"
-end
-
-desc "install gems"
-task :install do
-  sh "bundle install"
-end
--- tc_racional.rb
+++
@@ -1,8 +0,0 @@
-# Implementar en este fichero las Pruebas Unitarias asociadas a la clase Fraccion
-
-require "./racional.rb"
-require "test/unit"
-
-class Test_Fraccion < Test::Unit::TestCase
-
-end
